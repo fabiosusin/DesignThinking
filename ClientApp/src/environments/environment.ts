85dabd734cc99a5942364aaa0e74841490838d76
fabiosusisn <User@UCS-NOTE>
2022-06-21 20:14:32 -0300

    alterado enviroment

--- ClientApp/src/environments/environment.ts
+++ ClientApp/src/environments/environment.ts
@@ -4,6 +4,6 @@ import { Environment } from 'src/app/core/models/enum/environment.enum';
 export const environment = {
   production: false,
   name: Environment.dev,
-  apiBaseUrl: 'http://10.20.9.183:44363/v1/',
-  apiUrl: 'http://10.20.9.183:44363/v1/Intra/'
+  apiBaseUrl: 'http://localhost:44363/v1/',
+  apiUrl: 'http://localhost:44363/v1/Intra/'
 };
